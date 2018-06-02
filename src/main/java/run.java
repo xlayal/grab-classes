b21c61acf5c41abcac5117a1b91b26a0fd61d390
skyisbule <heqiut@qq.com>
2018-06-03 01:51:46 +0800

    修复了一下读文件的错误

--- src/main/java/run.java
+++ src/main/java/run.java
@@ -1,6 +1,8 @@
-import cn.hutool.core.io.file.FileReader;
-import cn.hutool.http.HttpUtil;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
@@ -8,51 +10,79 @@ import java.util.Map;
 
 public class run {
 
-    private static final String DATA_PATH = "c://Users//public//";
-    private static final String GRAB_URL = "http://10.10.240.34/xsxjs.aspx";
+    private static final String  DATA_PATH  = "c://Users//public//";
+    private static final String  GRAB_URL   = "http://10.10.240.34/xsxjs.aspx";
     private static final Integer SLEEP_TIME = 200;
-
-    private static Map<String, Object> getFileData(int i) {
-        String res = new FileReader(DATA_PATH + "data" + i + ".txt").readString();
-        Map<String, Object> map = new HashMap<String, Object>();
-        for (String column : res.split("\n")) {
-            String[] temp = column.split("::");
-            map.put(temp[0], temp[1]);
-        }
-        return map;
-    }
 
     private static class worker extends Thread {
 
-        int fileNum = 0;
+        private String readText(int i){
+            File file = new File(DATA_PATH + "data"+String.valueOf(i)+".txt");
+            StringBuilder res = new StringBuilder();
+            BufferedReader reader = null;
+            try {
+                reader = new BufferedReader(new java.io.FileReader(file));
+                String tempString = null;
+                while ((tempString = reader.readLine()) != null) {
+                    res.append(tempString+"\n");
+                }
+                reader.close();
+                return res.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "null";
+        }
 
-        private worker(int fileNum) {
-            this.fileNum = fileNum;
+        private HashMap<String, String> getFileData(int i) {
+            String res = readText(i);
+            HashMap<String, String> map = new HashMap<String,String>(16);
+            for (String temp : res.split("\n")){
+                String[] str = temp.split("::");
+                map.put(str[0],str[1]);
+            }
+            return map;
+        }
+
+        private String doPost(Map<String,String> param,String url,String Cookie) throws IOException {
+            Connection con = Jsoup.connect(url).timeout(6000);
+            param.forEach(con::data);
+            con.header("Cookie", Cookie);
+            Document doc = con.post();
+            return doc.toString();
+        }
+
+        int workerId = 0;
+
+        private worker(int id) {
+            this.workerId = id;
         }
 
         @Override
         public void run() {
-            System.out.println(fileNum);
-            int dataNum = fileNum;
+            int dataNum = workerId;
             //读取文件内容并转换成map
-            Map<String, Object> res = getFileData(dataNum);
-            res.forEach((k, v) -> {
-                System.out.println(k);
-            });
+            HashMap<String, String> param = getFileData(dataNum);
             //构造url
-            String url = GRAB_URL + "?xkkh=" + res.get("urlxkkh") + "&xh=" + res.get("xh");
+            String url = GRAB_URL + "?xkkh=" + param.get("urlxkkh") + "&xh=" + param.get("xh");
             //标记请求次数
             int count = 1;
             //请求的结果
-            String result;
+            String result = "";
+            String cookie = param.get("cookie");
+
             while (true) {
-                result = HttpUtil.post(url, res);
+                try {
+                    result = doPost(param,url,cookie);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 //简易获取请求返回的课程名称
                 String title = result.substring(result.indexOf("课程名称"), result.indexOf("课程名称") + 17);
                 //获取请求结果，即提取alert内容
                 result = result.substring(result.indexOf("alert(") + 7, result.indexOf("</script>") - 3);
                 //打印
-                System.out.println("工人:" + fileNum + " 返回结果：" + result + count + title);
+                System.out.println("工人:" + workerId + " 返回结果：" + result + count + title);
                 //抢课成功
                 if (result.indexOf("成功") > 0)
                     break;
@@ -69,6 +99,7 @@ public class run {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
+
         int fileCount = 1;
         while (new File(DATA_PATH + "data" + fileCount + ".txt").exists()) {
             new worker(fileCount).run();
